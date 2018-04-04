3f5ca7b72b32ac841821d3001d8b5d3139c3be89
Bala Krishna <prince.balakrishna@gmail.com>
2018-04-04 09:51:07 +0530

    Exercise 1-1

---
+++ Chapter - 1/1-1.c
@@ -0,0 +1,15 @@
+/*
+ 
+Excerise 1-1: Run the ``hello, world'' program on your system. Experiment with leaving
+out parts of the program, to see what error messages you get.
+ 
+Author: Bala Krishna (prince.balakrishna@gmail.com)
+ 
+*/
+ 
+#include <stdio.h>
+ 
+int main(void) {
+	printf("Hello World\n");
+	return 0;
+}
