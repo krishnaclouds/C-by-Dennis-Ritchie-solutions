4f0342a831585b68931bf581d3f2bb30bed0beba
Bala Krishna <prince.balakrishna@gmail.com>
2018-06-10 16:42:46 +0530

    1-9

---
+++ Chapter-1/1-9.c
@@ -0,0 +1,28 @@
+/*
+Excerise 1-9: 1-9. Write a program to copy its input to its output, replacing each
+string of one or more blanks by a single blank.
+
+Author: Bala Krishna (prince.balakrishna@gmail.com)
+
+*/
+
+#include <stdio.h>
+
+int main(void) {
+    int c, previous_blank;
+    previous_blank = 0;
+
+    while ((c = getchar()) != EOF){
+        if (c == ' '){
+            if (!previous_blank){
+                putchar(c);
+                previous_blank = 1;
+            }
+        } else {
+            previous_blank = 0;
+            putchar(c);
+        }
+    }
+
+    return 0;
+}
